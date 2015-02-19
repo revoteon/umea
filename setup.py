cb1a01d7e1f9c75fd121b99efd34e8abff6dc873
IG <igrar.guseynoff.com>
2015-02-19 18:39:39 +1100

    more info in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="umea",
     version="0.3b",
-    packages=["umea"],
     install_requires=[
         "python-ldap",
     ],
+    description=('Python LDAP ORM'),
+    packages=find_packages(exclude=['tests']),
 )
